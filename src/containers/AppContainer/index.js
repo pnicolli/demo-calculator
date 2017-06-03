b1236f0a28e4edf27f1905b960a0f65ac2a26e68
Piero Nicolli <piero.nicolli@yahoo.it>
2017-06-03 10:37:54 +0200

    Split view and container

--- src/containers/AppContainer/index.js
+++ src/containers/AppContainer/index.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import App from '../../views/App';
 
 class AppContainer extends Component {
+
+  initialState = () => ({
+    currentValue: 0,
+  });
+
+  constructor(props) {
+    super(props);
+    this.state = this.initialState();
+  }
+
+  resetState = () => {
+    this.setState(this.initialState);
+  };
+
+  invertSign = () => {
+    this.setState((prevState, props) => ({
+      currentValue: prevState.currentValue * (-1),
+    }));
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <App />
     );
   }
 }
---
+++ src/containers/AppContainer/index.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import AppContainer from './index';
+
+it('renders correctly', () => {
+  mount(<AppContainer />);
+});
